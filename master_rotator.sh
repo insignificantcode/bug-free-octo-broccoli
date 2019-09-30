2230ade57bc8a679b56b8f7fe99d12cacb5a6c6b
insignificantcode <hunt.nicholas@gmail.com>
2019-09-30 14:01:34 -0700

    Create master_rotator.sh

---
+++ master_rotator.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# this bad boy will check the db for read only status by `SELECT @@global.read_only;`
+# and if the read_only value is 0 we will rotate, other wise we won't.
+# pretty simple eh?
+# nh@nasa
+
+DB_READ=`mysql --login-path=backup -s -N -e "SELECT @@global.read_only;"`
+
+if (( $DB_READ == 0 ))
+   then
+        /usr/sbin/logrotate -f /usr/local/adm/mysql.rotate
+        /usr/bin/mysqladmin --login-path=backup flush-logs error slow general
+   else
+        exit 1
+fi
